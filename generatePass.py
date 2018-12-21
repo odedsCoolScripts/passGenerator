91913e4e78e79b9e41996806ab881679496cd72b
odedsCoolScripts <41224938+odedsCoolScripts@users.noreply.github.com>
2018-12-21 03:06:51 +0200

    Update generatePass.py
    Determine minimal lengh

--- generatePass.py
+++ generatePass.py
@@ -11,31 +11,46 @@ specialCharsList=["!","@","#","$","%","^","&","*","(",")","-","+","=","{","}","[
 def randomPass(length):
 	precentageList=[40,15,15,30]
 	random.shuffle(precentageList)
-	specialChars=(int(length)*precentageList[0])//100
-	capLetters=(int(length)*precentageList[1])//100
-	lowLetters=(int(length)*precentageList[2])//100
-	numbers=(int(length)*precentageList[3])//100
-	leftOver=int(length)-int(specialChars+capLetters+lowLetters+numbers)
-	if leftOver>0:
-		lowLetters=	lowLetters+leftOver
+	if int(length)>4:
+		specialChars=(int(length)*precentageList[0])//100
+		capLetters=(int(length)*precentageList[1])//100
+		lowLetters=(int(length)*precentageList[2])//100
+		numbers=(int(length)*precentageList[3])//100
+		leftOver=int(length)-int(specialChars+capLetters+lowLetters+numbers)
+		if leftOver>0:
+			lowLetters=	lowLetters+leftOver
+		charTypesList=[specialChars,capLetters,lowLetters,numbers]
+		i=0
+		while i < len(charTypesList):
+			if charTypesList[i]==0:
+				charTypesList[i]=1
+				n=0
+				while n < len(charTypesList):
+					if charTypesList[n] > 1:
+						charTypesList[n] -= 1
+						n=len(charTypesList)
+					n+=1
+			i+=1
+	else:
+		charTypesList=[1,1,1,1]
 	newList=[]
 	i=0
-	while i < specialChars:
+	while i < charTypesList[0]:
 		char=random.choice(specialCharsList)
 		newList.append(char)
 		i+=1
 	i=0
-	while i < capLetters:
+	while i < charTypesList[1]:
 		char=random.choice(string.ascii_uppercase)
 		newList.append(char)
 		i+=1
 	i=0
-	while i < lowLetters:
+	while i < charTypesList[2]:
 		char=random.choice(string.ascii_lowercase)
 		newList.append(char)
 		i+=1
 	i=0
-	while i < numbers:
+	while i < charTypesList[3]:
 		char=random.randint(0,9)
 		newList.append(char)
 		i+=1
